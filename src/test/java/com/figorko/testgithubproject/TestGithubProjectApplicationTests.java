fbcd48ae3943e464967a0195ffb353349f0c9e71
ihor.forys <figorko@gmail.com>
2023-11-17 17:51:57 +0100

    Initial commit

---
+++ src/test/java/com/figorko/testgithubproject/TestGithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.figorko.testgithubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
